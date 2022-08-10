ac25d055ae3c0ba014a1071880ac251c274df3bf
michaeljames24 <93047055+michaeljames24@users.noreply.github.com>
2022-08-10 17:07:35 +0100

    Fix bug on Timetable, and add modal to Personalised Timetable

--- screens/PersonalTimetableScreen.tsx
+++ screens/PersonalTimetableScreen.tsx
@@ -1,4 +1,100 @@
-import {View} from "react-native"
+import {View, ScrollView, Text, Pressable, Modal, Alert, StyleSheet, SectionList} from "react-native";
+import { useState } from "react";
+
 export default function PersonalTimetableScreen () {
-    return <View></View>
+
+    const [modalVisible, setModalVisible] = useState(false);
+
+
+    const DATA = [
+        {
+        title: "Friday",
+        data: ["Wolf Alice", "Fontaines D.C.", "Bad Boy Chiller Crew", "Arctic Monkeys", "Tame Impala", "The Mouse Outfit", "Khruangbin", "Too Many Zooz"]
+        },
+        {
+        title: "Saturday",
+        data: ["Wolf Alice", "Fontaines D.C.", "Bad Boy Chiller Crew", "Arctic Monkeys", "Tame Impala", "The Mouse Outfit", "Khruangbin", "Too Many Zooz"]
+        }
+    ];
+
+    return (
+      <ScrollView>
+        <Text>My Timetable</Text>
+        <Text>You haven't added any events yet.</Text>
+
+        <View style={styles.centeredView}>
+          <Modal
+            animationType="slide"
+            transparent={true}
+            visible={modalVisible}
+            onRequestClose={() => {
+              Alert.alert("Modal has been closed.");
+              setModalVisible(!modalVisible);
+            }}
+          >
+            <View style={styles.centeredView}>
+              <View style={styles.modalView}>
+                <Text style={styles.modalText}>Hello World!</Text>
+                <Pressable
+                  style={[styles.button, styles.buttonClose]}
+                  onPress={() => setModalVisible(!modalVisible)}
+                >
+                  <Text style={styles.textStyle}>Hide Modal</Text>
+                </Pressable>
+              </View>
+            </View>
+          </Modal>
+          <Pressable
+            style={[styles.button, styles.buttonOpen]}
+            onPress={() => setModalVisible(true)}
+          >
+            <Text style={styles.textStyle}>Add events</Text>
+          </Pressable>
+        </View>
+      </ScrollView>
+    );
 }
+
+const styles = StyleSheet.create({
+    centeredView: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+      marginTop: 22
+    },
+    modalView: {
+      margin: 20,
+      backgroundColor: "white",
+      borderRadius: 20,
+      padding: 35,
+      alignItems: "center",
+      shadowColor: "#000",
+      shadowOffset: {
+        width: 0,
+        height: 2
+      },
+      shadowOpacity: 0.25,
+      shadowRadius: 4,
+      elevation: 5
+    },
+    button: {
+      borderRadius: 20,
+      padding: 10,
+      elevation: 2
+    },
+    buttonOpen: {
+      backgroundColor: "#F194FF",
+    },
+    buttonClose: {
+      backgroundColor: "#2196F3",
+    },
+    textStyle: {
+      color: "white",
+      fontWeight: "bold",
+      textAlign: "center"
+    },
+    modalText: {
+      marginBottom: 15,
+      textAlign: "center"
+    }
+});
--- screens/TimetableScreen.tsx
+++ screens/TimetableScreen.tsx
@@ -1,4 +1,4 @@
-import { View, Button, Text, StyleSheet } from "react-native";
+import { View, Button, Text, StyleSheet, ScrollView } from "react-native";
 import { doc, getDoc } from "firebase/firestore";
 import { db } from "../firebase-config/firebase-config";
 import { useEffect, useState } from "react";
@@ -11,7 +11,7 @@ export default function TimetableScreen() {
     const myDoc = doc(db, "timetableCol", "timetablesDoc");
     getDoc(myDoc).then((snapshot) => {
       if (snapshot) {
-          setUserDoc(snapshot.data());
+        setUserDoc(snapshot.data());
       }
     });
   }
@@ -20,57 +20,74 @@ export default function TimetableScreen() {
     Read();
   }, []);
 
-  const friday_stage1_times = userDoc["roundhay-festival"]["friday"]["stage-1"]["times"];
-  const friday_stage2_times = userDoc["roundhay-festival"]["friday"]["stage-2"]["times"];
-  const saturday_stage1_times = userDoc["roundhay-festival"]["saturday"]["stage-1"]["times"];
-  const saturday_stage2_times = userDoc["roundhay-festival"]["saturday"]["stage-2"]["times"];
+  const friday_stage1_times = userDoc !== null ? 
+    userDoc["roundhay-festival"]["friday"]["stage-1"]["times"] : null;
+  const friday_stage2_times = userDoc !== null ? 
+    userDoc["roundhay-festival"]["friday"]["stage-2"]["times"] : null;
+  const saturday_stage1_times = userDoc !== null ? 
+    userDoc["roundhay-festival"]["saturday"]["stage-1"]["times"] : null;
+  const saturday_stage2_times = userDoc !== null ? 
+    userDoc["roundhay-festival"]["saturday"]["stage-2"]["times"] : null;
 
   return (
-      
-    <>
+    <ScrollView>
       <Text>Timetable</Text>
       <Text>Switch for Friday/Saturday</Text>
-       { userDoc !== null && friday_stage1_times.map((time) => {
-        return    (
-        <Card>
-        <Card.Content>
-          <Title>{time.artist}</Title>
-          <Paragraph>{time.time}</Paragraph>
-        </Card.Content>
-        <Card.Cover source={{ uri: 'https://picsum.photos/700' }} />
-      </Card>)
-    })}
-               { userDoc !== null && friday_stage2_times.map((time) => {
-        return    (
-        <Card>
-        <Card.Content>
-          <Title>{time.artist}</Title>
-          <Paragraph>{time.time}</Paragraph>
-        </Card.Content>
-        <Card.Cover source={{ uri: 'https://picsum.photos/700' }} />
-      </Card>)
-    })}
-           { userDoc !== null && saturday_stage1_times.map((time) => {
-        return    (
-        <Card>
-        <Card.Content>
-          <Title>{time.artist}</Title>
-          <Paragraph>{time.time}</Paragraph>
-        </Card.Content>
-        <Card.Cover source={{ uri: 'https://picsum.photos/700' }} />
-      </Card>)
-    })}
-           { userDoc !== null && saturday_stage2_times.map((time) => {
-        return    (
-        <Card>
-        <Card.Content>
-          <Title>{time.artist}</Title>
-          <Paragraph>{time.time}</Paragraph>
-        </Card.Content>
-        <Card.Cover source={{ uri: 'https://picsum.photos/700' }} />
-      </Card>)
-    })}
-    </>
+      <Text>Friday</Text>
+      <Text>Stage 1</Text>
+      {userDoc !== null &&
+        friday_stage1_times.map((time) => {
+          return (
+            <Card key={time.artist}>
+              <Card.Content>
+                <Title>{time.artist}</Title>
+                <Paragraph>{time.time}</Paragraph>
+              </Card.Content>
+              <Card.Cover source={{ uri: "https://picsum.photos/700" }} />
+            </Card>
+          );
+        })}
+      <Text>Stage 2</Text>
+      {userDoc !== null &&
+        friday_stage2_times.map((time) => {
+          return (
+            <Card key={time.artist}>
+              <Card.Content>
+                <Title>{time.artist}</Title>
+                <Paragraph>{time.time}</Paragraph>
+              </Card.Content>
+              <Card.Cover source={{ uri: "https://picsum.photos/700" }} />
+            </Card>
+          );
+        })}
+      <Text>Saturday</Text>
+      <Text>Stage 1</Text>
+      {userDoc !== null &&
+        saturday_stage1_times.map((time) => {
+          return (
+            <Card key={time.artist}>
+              <Card.Content>
+                <Title>{time.artist}</Title>
+                <Paragraph>{time.time}</Paragraph>
+              </Card.Content>
+              <Card.Cover source={{ uri: "https://picsum.photos/700" }} />
+            </Card>
+          );
+        })}
+      <Text>Stage 2</Text>
+      {userDoc !== null &&
+        saturday_stage2_times.map((time) => {
+          return (
+            <Card key={time.artist}>
+              <Card.Content>
+                <Title>{time.artist}</Title>
+                <Paragraph>{time.time}</Paragraph>
+              </Card.Content>
+              <Card.Cover source={{ uri: "https://picsum.photos/700" }} />
+            </Card>
+          );
+        })}
+    </ScrollView>
   );
 }
 
@@ -78,6 +95,4 @@ const styles = StyleSheet.create({
     Card: {
       backgroundColor: 'red',
     },
-  });
-
-
+});
