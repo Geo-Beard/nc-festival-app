175bddc37f4d14b08efba77d81a4024dc20aef4a
woodsjd11 <woodsjd@hotmail.co.uk>
2022-08-11 17:55:03 +0100

    add list of photos display on photosscreen

--- screens/PhotosScreen.tsx
+++ screens/PhotosScreen.tsx
@@ -1,13 +1,58 @@
-import * as React from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { useEffect, useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  Image,
+  ScrollView,
+} from "react-native";
+//retrieve data
+import { db } from "../firebase-config/firebase-config";
+import {
+  collection,
+  DocumentData,
+  getDocs,
+  Timestamp,
+} from "firebase/firestore";
 
-export default function PhotosScreen({navigation}) {
+export default function PhotosScreen({ navigation }) {
+  const [photos, setPhotos] = useState<DocumentData | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const retrievedDocuments = async () => {
+    const querySnapshot = await getDocs(collection(db, "festivalImages"));
+    const photoArray = [];
+    setIsLoading(false);
+    querySnapshot.forEach((doc) => {
+      photoArray.push(doc.data());
+      setPhotos([...photoArray]);
+    });
+  };
+
+  useEffect(() => {
+    setIsLoading(true);
+    retrievedDocuments();
+  }, []);
+
   return (
     <>
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Text>Photos Screen</Text>
-        <Button onPress={() => navigation.navigate("UploadPhoto")} title="Upload photo" />
-      </View>
+      <Button
+        onPress={() => navigation.navigate("UploadPhoto")}
+        title="Upload photo"
+      />
+      <ScrollView>
+        {!isLoading &&
+          photos &&
+          photos.map((photo) => {
+            return (
+              <Image
+                source={{ uri: photo.imageUrl }}
+                style={{ width: "90%", height: 200 }}
+              />
+            );
+          })}
+      </ScrollView>
     </>
   );
 }
