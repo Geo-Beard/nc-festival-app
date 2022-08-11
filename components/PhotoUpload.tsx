9892dffd07faaf1e1ff2b82987de844b947b306c
woodsjd11 <woodsjd@hotmail.co.uk>
2022-08-11 10:31:10 +0100

    add permission state to enable/disable the select photo button

--- components/PhotoUpload.tsx
+++ components/PhotoUpload.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Button, Image, View, Alert } from "react-native";
+import { Button, Image, View, Alert, Platform } from "react-native";
 //select image
 import * as ImagePicker from "expo-image-picker";
 //upload image to firebase
@@ -7,25 +7,29 @@ import { getStorage, ref, uploadBytes } from "firebase/storage";
 
 export default function PhotoUpload() {
   const [image, setImage] = useState<string | undefined>();
+  const [hasPermission, setHasPermission] = useState(false);
   // const [uploading, setUploading] = useState(false);
 
-  // useEffect(() => {
-  //   (async () => {
-  //     if (Platform.OS !== "web") {
-  //       const { status } =
-  //         await ImagePicker.requestMediaLibraryPermissionsAsync();
-  //       if (status !== "granted") {
-  //         Alert("sorry, we need camera roll permissions to make this work!");
-  //       }
-  //     }
-  //   })();
-  // }, []);
+  useEffect(() => {
+    (async () => {
+      if (Platform.OS !== "web") {
+        const { status } =
+        await ImagePicker.requestMediaLibraryPermissionsAsync();
+        setHasPermission(status === "granted")
+        if (status !== "granted") {
+          Alert.alert(
+            "sorry, we need camera roll permissions to make this work!"
+          );
+        }
+      }
+    })();
+  }, []);
 
   const pickImage = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.Images,
       allowsEditing: true,
-        aspect: [4, 3],
+      aspect: [4, 3],
       quality: 1,
     });
 
@@ -37,61 +41,19 @@ export default function PhotoUpload() {
       //how the image will be addressed inside the storage
       const imageRef = ref(storage, "image.jpg");
       //convert image to array of bytes
-      const img = await fetch(result.uri)
-      const bytes = await img.blob()
+      const img = await fetch(result.uri);
+      const bytes = await img.blob();
       //upload images
       await uploadBytes(imageRef, bytes);
     }
   };
-  // const uploadImage = async () => {
-  //   if (image) {
-  //   }
-  //   // https://github.com/expo/expo/issues/2402#issuecomment-443726662
-  //   const blob = await new Promise((resolve, reject) => {
-  //     const xhr = new XMLHttpRequest();
-  //     xhr.onload = function () {
-  //       resolve(xhr.response);
-  //     };
-  //     xhr.onerror = function () {
-  //       reject(new TypeError("Network request failed"));
-  //     };
-  //     xhr.responseType = "blob";
-  //     xhr.open("GET", image, true);
-  //     xhr.send(null);
-  //   });
-  //   const storage = getStorage();
-  //   const imageRef = ref(storage, "imagepic.jpg");
-  //   const storageRef = ref(storage, "some picture");
-  //   // const ref = firebase.storage().ref().child(new Date().toISOString());
-  //   const snapshot = ref.put(blob);
-  //   snapshot.on(
-  //     firebase.storage.TaskEvent.STATE_CHANGED,
-  //     () => {
-  //       setUploading(true);
-  //     },
-  //     (error: string) => {
-  //       setUploading(false);
-  //       console.log(error);
-  //       blob.close();
-  //       return;
-  //     },
-  //     () => {
-  //       snapshot.snapshot.ref.getDownloadURL().then((url) => {
-  //         setUploading(false);
-  //         console.log(url);
-  //         blob.close();
-  //         return url;
-  //       });
-  //     }
-  //   );
-  // };
 
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       {image && (
         <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />
       )}
-      <Button title="Select Photo" onPress={pickImage} />
+      <Button title="Select Photo" onPress={pickImage} disabled={!hasPermission}/>
       {/* <Button title="Upload Photo" onPress={uploadImage} /> */}
     </View>
   );
