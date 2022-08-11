e53768802f76c002716a23a2a75774a89cfd4783
michaeljames24 <93047055+michaeljames24@users.noreply.github.com>
2022-08-11 12:06:04 +0100

    Link personal timetables with Firebase.

--- App.tsx
+++ App.tsx
@@ -22,7 +22,7 @@ export default function App() {
   return (
     <UserContext.Provider value={user}>
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Timetable">
+      <Stack.Navigator initialRouteName="PersonalTimetable">
         <Stack.Screen name="Signup" component={SignupScreen} />
         <Stack.Screen name="Login" component={LoginScreen}/>
         <Stack.Screen name="Map" component={MapScreen} />
--- screens/PersonalTimetableScreen.tsx
+++ screens/PersonalTimetableScreen.tsx
@@ -1,26 +1,64 @@
-import {View, ScrollView, Text, Pressable, Modal, Alert, StyleSheet, SectionList} from "react-native";
+import {View, ScrollView, Text, Pressable, Modal, Alert, StyleSheet, SectionList, StatusBar} from "react-native";
 import { useState } from "react";
+import { db } from '../firebase-config/firebase-config';
+import { doc, getDoc, setDoc } from 'firebase/firestore';
+import { getAuth } from "firebase/auth";
 
 export default function PersonalTimetableScreen () {
 
     const [modalVisible, setModalVisible] = useState(false);
+    const [userTimetable, setUserTimetable] = useState([]);
+    const auth = getAuth();
+    const userID = auth.currentUser ? auth.currentUser.uid : null;
 
+    function Create() {
+      if (userID !== null) {
+        const myDoc = doc(db,"userTimetables", userID.toString());
+        const docData = {
+          "timetable": userTimetable
+        };
+        setDoc(myDoc,docData)
+            .catch(() => {
+                Alert.alert("Something went wrong. Please re-add your events.");
+            })
+      }
+    }
 
     const DATA = [
         {
         title: "Friday",
-        data: ["Wolf Alice", "Fontaines D.C.", "Bad Boy Chiller Crew", "Arctic Monkeys", "Tame Impala", "The Mouse Outfit", "Khruangbin", "Too Many Zooz"]
+        data: [["Wolf Alice", 1300], ["Fontaines D.C.", 1400], ["Bad Boy Chiller Crew", 1500], ["Arctic Monkeys", 1600], ["Tame Impala", 1300], ["The Mouse Outfit", 1400], ["Khruangbin", 1500], ["Too Many Zooz", 1600]]
         },
         {
         title: "Saturday",
-        data: ["Wolf Alice", "Fontaines D.C.", "Bad Boy Chiller Crew", "Arctic Monkeys", "Tame Impala", "The Mouse Outfit", "Khruangbin", "Too Many Zooz"]
+        data: [["Foals", 1300], ["The Libertines", 1400], ["Idles", 1500], ["Hak Baker", 1600], ["Lianne La Havas", 1300], ["Skunk Anansie", 1400], ["Metronomy", 1500], ["Haim", 1600]]
         }
     ];
+
+    const Item = ({ title }) => (
+      <View style={styles.item}>
+        <Text style={styles.title}>{title[0]}</Text>
+        <Text>{title[1]}</Text>
+        <Pressable
+          disabled={false}
+          style={[styles.button, styles.buttonClose]}
+          onPress={() => {
+            if (!userTimetable.includes(title[0])) {
+              setUserTimetable([...userTimetable, title[0]]);
+            }
+          }}
+        >
+          <Text style={styles.textStyle}>Add</Text>
+        </Pressable>
+      </View>
+    );
 
     return (
       <ScrollView>
         <Text>My Timetable</Text>
-        <Text>You haven't added any events yet.</Text>
+        {userTimetable.length === 0 && <Text>You haven't added any events yet.</Text>}
+        {userTimetable.length !== 0 && userTimetable.map(event => {return (<Text key={event}>{event}</Text>)})}
+
 
         <View style={styles.centeredView}>
           <Modal
@@ -34,12 +72,26 @@ export default function PersonalTimetableScreen () {
           >
             <View style={styles.centeredView}>
               <View style={styles.modalView}>
-                <Text style={styles.modalText}>Hello World!</Text>
+                <Text style={styles.modalText}>Add artists to your timetable:</Text>
+                <View style={styles.container}>
+                  <SectionList
+                    sections={DATA}
+                    keyExtractor={(item, index) => item + index}
+                    renderItem={({ item }) => <Item title={item} />}
+                    renderSectionHeader={({ section: { title } }) => (
+                      <Text style={styles.header}>{title}</Text>
+                    )}
+                  />
+                </View>
                 <Pressable
                   style={[styles.button, styles.buttonClose]}
-                  onPress={() => setModalVisible(!modalVisible)}
+                  onPress={() => {
+                    setModalVisible(!modalVisible);
+                    Create();
+                    console.log(`events added to ${userID}'s timetable on Firebase.`);
+                  }}
                 >
-                  <Text style={styles.textStyle}>Hide Modal</Text>
+                  <Text style={styles.textStyle}>Add</Text>
                 </Pressable>
               </View>
             </View>
@@ -56,6 +108,8 @@ export default function PersonalTimetableScreen () {
 }
 
 const styles = StyleSheet.create({
+    // MODAL:
+
     centeredView: {
       flex: 1,
       justifyContent: "center",
@@ -96,5 +150,25 @@ const styles = StyleSheet.create({
     modalText: {
       marginBottom: 15,
       textAlign: "center"
+    },
+
+    // SECTION LIST:
+
+    container: {
+      flex: 1,
+      paddingTop: StatusBar.currentHeight,
+      marginHorizontal: 16
+    },
+    item: {
+      backgroundColor: "#f9c2ff",
+      padding: 20,
+      marginVertical: 8
+    },
+    header: {
+      fontSize: 32,
+      backgroundColor: "#fff"
+    },
+    title: {
+      fontSize: 24
     }
 });
