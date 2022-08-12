37ad04c3e7ef3f18d29fc00d0b04ea2b16448e96
Anna Dehl <annadehl@Annas-Macbook-Air.local>
2022-08-12 14:19:50 +0100

    display cards for new timetable data on timetable screen

--- screens/TimetableScreen.tsx
+++ screens/TimetableScreen.tsx
@@ -8,7 +8,7 @@ export default function TimetableScreen() {
   const [userDoc, setUserDoc] = useState(null);
 
   function Read() {
-    const myDoc = doc(db, "timetableCol", "timetablesDoc");
+    const myDoc = doc(db, "events", "artists");
     getDoc(myDoc).then((snapshot) => {
       if (snapshot) {
         setUserDoc(snapshot.data());
@@ -20,34 +20,44 @@ export default function TimetableScreen() {
     Read();
   }, []);
 
-  const friday_stage1_times = userDoc !== null ? 
-    userDoc["roundhay-festival"]["friday"]["stage-1"]["times"] : null;
-  const friday_stage2_times = userDoc !== null ? 
-    userDoc["roundhay-festival"]["friday"]["stage-2"]["times"] : null;
-  const saturday_stage1_times = userDoc !== null ? 
-    userDoc["roundhay-festival"]["saturday"]["stage-1"]["times"] : null;
-  const saturday_stage2_times = userDoc !== null ? 
-    userDoc["roundhay-festival"]["saturday"]["stage-2"]["times"] : null;
+  // const friday_stage1_times =
+  //   userDoc !== null
+  //     ? userDoc["roundhay-festival"]["friday"]["stage-1"]["times"]
+  //     : null;
+  // const friday_stage2_times =
+  //   userDoc !== null
+  //     ? userDoc["roundhay-festival"]["friday"]["stage-2"]["times"]
+  //     : null;
+  // const saturday_stage1_times =
+  //   userDoc !== null
+  //     ? userDoc["roundhay-festival"]["saturday"]["stage-1"]["times"]
+  //     : null;
+  // const saturday_stage2_times =
+  //   userDoc !== null
+  //     ? userDoc["roundhay-festival"]["saturday"]["stage-2"]["times"]
+  //     : null;
+
+  const dataArray = userDoc !== null ? Object.values(userDoc) : null;
 
   return (
     <ScrollView>
+      {userDoc !== null && console.log(Object.values(userDoc))}
       <Text>Timetable</Text>
-      <Text>Switch for Friday/Saturday</Text>
       <Text>Friday</Text>
       <Text>Stage 1</Text>
-      {userDoc !== null &&
-        friday_stage1_times.map((time) => {
+      {dataArray !== null &&
+        dataArray.map((artist) => {
           return (
-            <Card key={time.artist}>
+            <Card key={artist.name}>
               <Card.Content>
-                <Title>{time.artist}</Title>
-                <Paragraph>{time.time}</Paragraph>
+                <Title>{artist.name}</Title>
+                <Paragraph>{artist.time}</Paragraph>
               </Card.Content>
-              <Card.Cover source={{ uri: "https://picsum.photos/700" }} />
+              <Card.Cover source={{ uri: artist.image }} />
             </Card>
           );
         })}
-      <Text>Stage 2</Text>
+      {/* <Text>Stage 2</Text>
       {userDoc !== null &&
         friday_stage2_times.map((time) => {
           return (
@@ -86,13 +96,13 @@ export default function TimetableScreen() {
               <Card.Cover source={{ uri: "https://picsum.photos/700" }} />
             </Card>
           );
-        })}
+        })} */}
     </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-    Card: {
-      backgroundColor: 'red',
-    },
+  Card: {
+    backgroundColor: "red",
+  },
 });
