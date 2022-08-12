a5868bd7733d27e2f2ca3c7ff460b8be8bec0f09
woodsjd11 <woodsjd@hotmail.co.uk>
2022-08-12 16:06:05 +0100

    add ability to like and unlike photos per user

--- components/LikeButton.tsx
+++ components/LikeButton.tsx
@@ -1,8 +1,16 @@
 import { Button } from "react-native";
 //retrieve data
 import { db } from "../firebase-config/firebase-config";
-import { doc, updateDoc, increment, arrayUnion } from "firebase/firestore";
+import {
+  doc,
+  updateDoc,
+  increment,
+  arrayUnion,
+  getDoc,
+  arrayRemove,
+} from "firebase/firestore";
 import { getAuth } from "firebase/auth";
+import { useEffect } from "react";
 
 interface photosProp {
   photoId: string;
@@ -13,14 +21,28 @@ export default function LikeButton({ photoId }: photosProp) {
   const auth = getAuth();
   const user = auth.currentUser;
 
+  useEffect(() => {});
+
   const handleLike = async () => {
     try {
       const imageDocRef = doc(db, "festivalImages", photoId);
 
-      await updateDoc(imageDocRef, {
-        likes: increment(1),
-        likedByUsers: arrayUnion(user?.uid),
-      });
+      const docSnap = await getDoc(imageDocRef);
+      const likedByUser = docSnap.data()?.likedByUsers.includes(user?.uid);
+      const likeIncrement = likedByUser ? -1 : 1;
+
+      if (likedByUser) {
+        await updateDoc(imageDocRef, {
+          likes: increment(likeIncrement),
+          likedByUsers: arrayRemove(user?.uid),
+        });
+      }
+      if (!likedByUser) {
+        await updateDoc(imageDocRef, {
+          likes: increment(likeIncrement),
+          likedByUsers: arrayUnion(user?.uid),
+        });
+      }
     } catch (e) {
       //error occurs when navigating to photos with an already logged in user on initial app load. Navigating away and back to the photos page sorts the issue. But should check this out when we have time
       console.log("error----->", e);
