b65421b63b89c6bfded8b4467f3f0b3934817ea9
woodsjd11 <104032001+woodsjd11@users.noreply.github.com>
2022-08-15 12:05:18 +0100

    add friends functionality inc firebase database

--- screens/FriendsScreen.tsx
+++ screens/FriendsScreen.tsx
@@ -1,29 +1,54 @@
 import { getAuth } from "firebase/auth";
 import { db } from "../firebase-config/firebase-config";
-import { doc, setDoc } from "@firebase/firestore";
+import {
+  doc,
+  getDocs,
+  collection,
+  query,
+  where,
+  updateDoc,
+  arrayUnion,
+} from "@firebase/firestore";
 import { Button, TextInput } from "react-native";
+import { useState } from "react";
 
-export default function FriendsScreen() {
+export default function FriendsScreen({ navigation }: any) {
+  const [email, setEmail] = useState<string | null>(null);
+
   const auth = getAuth();
   const user = auth.currentUser;
 
-  const updateFriends = async () => {
+  const updateFriends = async (friendUid: string) => {
     try {
-      await setDoc(doc(db, "users", `${user?.uid}`), {
-        friends: [],
+      await updateDoc(doc(db, "users", `${user?.uid}`), {
+        friends: arrayUnion(friendUid),
       });
     } catch (e) {
       console.log(e);
     }
   };
 
-  const fetchFriendUid = () => {
+  const fetchFriendUid = async () => {
+    const usersRef = collection(db, "users");
+    const q = query(usersRef, where("userEmail", "==", `${email}`));
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      updateFriends(doc.id);
+    });
+  };
+
+  const handleEmail = (emailInput: string) => {
+    setEmail(emailInput);
   };
 
   return (
     <>
-      <TextInput placeholder="Email" accessibilityLabel="Email" />
-      <Button title="Submit" />
+      <TextInput
+        onChangeText={handleEmail}
+        placeholder="Email"
+        accessibilityLabel="Email"
+      />
+      <Button title="Submit" onPress={fetchFriendUid} />
     </>
   );
 }
