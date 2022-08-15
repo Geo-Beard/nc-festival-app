5aed077b533dcced8883c71ec9270e4cc2a62611
michaeljames24 <93047055+michaeljames24@users.noreply.github.com>
2022-08-15 10:37:00 +0100

    Separate stack and tab navigators.

--- screens/FriendsScreen.tsx
+++ screens/FriendsScreen.tsx
@@ -1,6 +1,12 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+import {FontAwesome5} from '@expo/vector-icons';
+
 export default function FriendsScreen() {
     return (
-        <>
-        </>
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+          <Text>Friends Screen</Text>
+          <FontAwesome5 name="user-friends" size={24} color="black" />
+        </View>
     );
 };
