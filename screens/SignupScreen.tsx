ac7d89b2aa358bcdbc14db04167f6bbf263f50aa
michaeljames24 <93047055+michaeljames24@users.noreply.github.com>
2022-08-16 17:33:59 +0100

    Add userId to Firebase db on sign up

--- screens/SignupScreen.tsx
+++ screens/SignupScreen.tsx
@@ -81,6 +81,7 @@ export default function LoginScreen({ navigation }: any) {
             setDoc(doc(db, "users", `${user?.uid}`), {
               friends: [],
               userEmail: email.email,
+              userId: user?.uid
             });
           })
           .then(() => {
