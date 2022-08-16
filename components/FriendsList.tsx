842df5b413584639fc79fdfc9ac9968e9c15fc0f
woodsjd11 <104032001+woodsjd11@users.noreply.github.com>
2022-08-16 09:51:54 +0100

    require user context to fix async issues on first render, before continuing this branch

---
+++ components/FriendsList.tsx
@@ -0,0 +1,29 @@
+import { getAuth } from "firebase/auth";
+import { db } from "../firebase-config/firebase-config";
+import { doc, getDoc, DocumentData } from "@firebase/firestore";
+import { Button, TextInput, Text } from "react-native";
+import { useEffect, useState } from "react";
+import { showMessage } from "react-native-flash-message";
+
+export default function FriendsList() {
+  //friends that have access to the users location data
+  const [friendsAccessUid, setFriendsAccessUid] = useState(null);
+  //current user uid
+  const auth = getAuth();
+  const user = auth.currentUser;
+
+  useEffect(() => {
+    //fetch users friends to which the user has access to their locations
+    getDoc(doc(db, "users", `${user?.uid}`))
+      .then((docSnap) => {
+        const userData = docSnap.data();
+        return userData?.friends;
+      })
+      .then((usersUidData) => {
+        console.log(usersUidData);
+        setFriendsAccessUid(usersUidData);
+      });
+  }, []);
+
+  return <></>;
+}
