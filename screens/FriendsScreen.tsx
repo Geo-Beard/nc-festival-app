d8ec9902e9c31b32bf75383faee4abd3bc474b36
woodsjd11 <104032001+woodsjd11@users.noreply.github.com>
2022-08-16 11:54:30 +0100

    solve async issues with retrieving current user data on first render

--- screens/FriendsScreen.tsx
+++ screens/FriendsScreen.tsx
@@ -1,12 +1,35 @@
 import { SafeAreaView } from "react-native-safe-area-context";
 import AddFriends from "../components/AddFriends";
 import FriendsList from "../components/FriendsList";
+import { getAuth } from "firebase/auth";
+import { useEffect, useState } from "react";
+import { Text } from "react-native";
 
 export default function FriendsScreen({ navigation }: any) {
+  const [currentUser, setCurrentUser] = useState<string | null>(null);
+
+  function checkAuthStatus() {
+    return new Promise((resolve, reject) => {
+      try {
+        getAuth().onAuthStateChanged((user) => {
+          // console.log("userChecked:", user.uid);
+          setCurrentUser(user);
+          resolve(user);
+        });
+      } catch {
+        reject("api failed");
+      }
+    });
+  }
+
+  useEffect(() => {
+    checkAuthStatus();
+  }, []);
+
   return (
     <SafeAreaView style={{ flex: 1, alignItems: "center" }}>
       <AddFriends />
-      <FriendsList />
+      {currentUser && <FriendsList user={currentUser} />}
     </SafeAreaView>
   );
 }
