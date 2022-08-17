af6d2013ab79ad4bffb313684caf2763814a439c
woodsjd11 <104032001+woodsjd11@users.noreply.github.com>
2022-08-17 09:03:33 +0100

    merge conflicts resolved

--- components/FriendsList.tsx
+++ components/FriendsList.tsx
@@ -1,13 +1,20 @@
 import { getAuth } from "firebase/auth";
 import { db } from "../firebase-config/firebase-config";
-import { doc, getDoc, DocumentData } from "@firebase/firestore";
+import {
+  doc,
+  getDoc,
+  getDocs,
+  query,
+  collection,
+  where,
+} from "@firebase/firestore";
 import { Button, TextInput, Text } from "react-native";
 import { useEffect, useState } from "react";
 import { showMessage } from "react-native-flash-message";
 
-export default function FriendsList({user}:any) {
+export default function FriendsList({ user }: any) {
   //friends that have access to the users location data
-  const [friendsAccessUid, setFriendsAccessUid] = useState(null);
+  // const [friendsAccessUid, setFriendsAccessUid] = useState(null);
 
   useEffect(() => {
     //fetch users friends to which the user has access to their locations
@@ -17,8 +24,19 @@ export default function FriendsList({user}:any) {
         return userData?.friends;
       })
       .then((usersUidData) => {
-        console.log(usersUidData);
-        setFriendsAccessUid(usersUidData);
+        // console.log(usersUidData);
+        // const q = query(
+        //   collection(db, "users"),
+        //   where("capital", "in", [...usersUidData])
+        // );
+        // return getDocs(q);
+        return getDocs(collection(db, "users"));
+      })
+      .then((docs) => {
+        console.log(docs);
+        docs.forEach((doc) => {
+          console.log(doc);
+        });
       });
   }, []);
 
--- screens/ProfileScreen.tsx
+++ screens/ProfileScreen.tsx
@@ -1,64 +1,84 @@
 import { useEffect, useState } from "react";
 import { getAuth } from "firebase/auth";
-import { Text, SafeAreaView, StyleSheet, View} from 'react-native';
-import { Avatar } from 'react-native-paper';
-import { TouchableOpacity } from 'react-native';
-import { Entypo } from '@expo/vector-icons';
+import { Text, SafeAreaView, StyleSheet, View } from "react-native";
+import { Avatar } from "react-native-paper";
+import { TouchableOpacity } from "react-native";
+import { Entypo } from "@expo/vector-icons";
 
 export default function ProfileScreen({ navigation }: any) {
-    const [isLoading, setIsLoading] = useState(true);
-    const [error, setError] = useState(false);
-    const auth = getAuth();
-    const user = auth.currentUser;
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const auth = getAuth();
+  const user = auth.currentUser;
 
-    useEffect(() => {
-      setIsLoading(false);
-    }, [])
+  useEffect(() => {
+    setIsLoading(false);
+  }, []);
 
-    function signOut() {
-      setIsLoading(true);
-      auth.signOut()
-        .then(() => {
-          console.log("Sign out successful");
-          setIsLoading(false);
-          navigation.navigate("Signup");
-        })
-        .catch((error) => {
-          setError(error);
-          console.log(error);
-          setIsLoading(false);
-        });
-    }
+  function signOut() {
+    setIsLoading(true);
+    auth
+      .signOut()
+      .then(() => {
+        console.log("Sign out successful");
+        setIsLoading(false);
+        navigation.navigate("Signup");
+      })
+      .catch((error) => {
+        setError(error);
+        console.log(error);
+        setIsLoading(false);
+      });
+  }
 
-    return isLoading ? (<SafeAreaView style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}><Text>Loading...</Text></SafeAreaView>) : 
-    !user ? (<SafeAreaView style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}><Text>Please reload the app and sign in.</Text></SafeAreaView>) : (
-        <SafeAreaView style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-          <Text style={styles.header}>Hello, {user?.displayName}.</Text>
-          <Avatar.Image size={120} source={require('../assets/img/avatar_001.jpg')} style={styles.avatar} />
-          <TouchableOpacity onPress={signOut}>
-            <View style={{flexDirection: 'row', alignItems: 'center'}}>
-                <Entypo name='log-out' size={24} color='black' />
-                <Text style={{
-                  fontSize: 15,
-                  marginLeft: 5
-                }}
-                >Sign Out</Text>
-            </View>
-          </TouchableOpacity>
-          {error && <Text>Unable to sign out. Please try again.</Text>}
-        </SafeAreaView>
-      );
-
+  return isLoading ? (
+    <SafeAreaView
+      style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+    >
+      <Text>Loading...</Text>
+    </SafeAreaView>
+  ) : !user ? (
+    <SafeAreaView
+      style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+    >
+      <Text>Please reload the app and sign in.</Text>
+    </SafeAreaView>
+  ) : (
+    <SafeAreaView
+      style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+    >
+      <Text style={styles.header}>Hello, {user?.displayName}.</Text>
+      <Avatar.Image
+        size={120}
+        source={require("../assets/img/avatar_001.jpg")}
+        style={styles.avatar}
+      />
+      <TouchableOpacity onPress={signOut}>
+        <View style={{ flexDirection: "row", alignItems: "center" }}>
+          <Entypo name="log-out" size={24} color="black" />
+          <Text
+            style={{
+              fontSize: 15,
+              marginLeft: 5,
+            }}
+          >
+            Sign Out
+          </Text>
+        </View>
+      </TouchableOpacity>
+      {error && <Text>Unable to sign out. Please try again.</Text>}
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
   header: {
     fontSize: 40,
-    justifyContent: 'center',
-    marginBottom: 30
+    justifyContent: "center",
+    marginBottom: 30,
   },
   avatar: {
-    justifyContent: 'center',
-    marginBottom: 75
-  }
+    justifyContent: "center",
+    marginBottom: 75,
+  },
 });
